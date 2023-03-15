93c3280b73011759bdd7ae4a16bb3daad5b2000e
sanaeas <sanaassim1@gmail.com>
2023-03-15 16:25:41 +0100

    Add Navbar and Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./Components/Nav";
+import Home from "./Components/Home";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <Router>
+            <div className="app">
+                <Nav />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
