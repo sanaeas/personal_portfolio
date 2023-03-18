2bf11357ef83662852aecfc87cc149fb5189c4a8
sanaeas <sanaassim1@gmail.com>
2023-03-18 09:30:56 +0100

    Remove the header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,51 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ArrowForwardRoundedIcon from "@mui/icons-material/ArrowForwardRounded";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import Logo from "../images/logo.png";
 
 function Home() {
     return (
         <div>
-            <header className="header">
-                <Link className="header__logo" to="/">
-                    <img src={Logo} alt="" />
-                </Link>
-                <div className="header__links">
-                    <button className="header__link">
-                        <GitHubIcon className="header__link--icon" />
-                    </button>
-                    <button className="header__link">
-                        <LinkedInIcon className="header__link--icon" />
-                    </button>
-                </div>
-            </header>
-            <div>
-                <main className="home">
-                    <div className="home__container">
-                        <div>
-                            <h1>
-                                HEY, I’M <span> SANAE </span>
-                            </h1>
-                        </div>
-                        <div>
-                            <h1>
-                                I’M A <span> FRONT-END DEVELOPER </span>
-                            </h1>
-                        </div>
-                        <div>
-                            <p>&amp; a Software Engineer Student</p>
-                        </div>
-                        <h3>
-                            <Link to="/about">
-                                <ArrowForwardRoundedIcon />
-                                <span>About me</span>
-                            </Link>
-                        </h3>
+            <main className="home">
+                <div className="home__container">
+                    <div>
+                        <h1>
+                            HEY, I’M <span> SANAE </span>
+                        </h1>
                     </div>
-                </main>
-            </div>
+                    <div>
+                        <h1>
+                            I’M A <span> FRONT-END DEVELOPER </span>
+                        </h1>
+                    </div>
+                    <div>
+                        <p>&amp; a Software Engineering Student</p>
+                    </div>
+                    <h3>
+                        <Link to="/about">
+                            <ArrowForwardRoundedIcon />
+                            <span>About me</span>
+                        </Link>
+                    </h3>
+                </div>
+            </main>
         </div>
     );
 }
