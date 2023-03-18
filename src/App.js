85cddb880b8a0355f5557d597018db03d65c6d86
sanaeas <sanaassim1@gmail.com>
2023-03-18 13:03:49 +0100

    Add Projects page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from "./components/Nav";
 import Home from "./components/Home";
 import About from "./components/About";
 import Header from "./components/Header";
+import Projects from "./components/Projects";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
                 </Routes>
             </div>
         </Router>
