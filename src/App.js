876f66950b0523a03260765618e35634830a7a2d
sanaeas <sanaassim1@gmail.com>
2023-03-18 09:29:44 +0100

    Add Header and About components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
+import About from "./components/About";
+import Header from "./components/Header";
 
 function App() {
     return (
         <Router>
             <div className="app">
                 <Nav />
+                <Header />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
                 </Routes>
             </div>
         </Router>
