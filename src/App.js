03859d3b83c63c69941abf434b74a71444257e45
sanaeas <sanaassim1@gmail.com>
2023-03-20 10:08:35 +0000

    Add contact page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/projects" element={<Projects />} />
+                    <Route path="/contact" element={<Contact />} />
                 </Routes>
             </div>
         </Router>
