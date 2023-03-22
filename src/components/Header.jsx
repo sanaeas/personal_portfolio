dfe7f655d914c4e14e5082ad1f00ca0d8023db93
sanaeas <sanaassim1@gmail.com>
2023-03-22 08:56:34 +0000

    Add links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,12 +11,22 @@ function Header() {
                 <img src={Logo} alt="" />
             </Link>
             <div className="header__links">
-                <button className="header__link">
-                    <GitHubIcon className="header__link--icon" />
-                </button>
-                <button className="header__link">
-                    <LinkedInIcon className="header__link--icon" />
-                </button>
+                <Link
+                    to="https://github.com/sanaeas"
+                    target="_blank"
+                    rel="noreferrer">
+                    <button className="header__link">
+                        <GitHubIcon className="header__link--icon" />
+                    </button>
+                </Link>
+                <Link
+                    to="https://www.linkedin.com/in/sana-assim-a2822426b/"
+                    target="_blank"
+                    rel="noreferrer">
+                    <button className="header__link">
+                        <LinkedInIcon className="header__link--icon" />
+                    </button>
+                </Link>
             </div>
         </header>
     );
